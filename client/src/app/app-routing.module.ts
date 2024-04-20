26bca733df4ef0561f6ee41f13327511d17e8dc7
Minh Quan <steaky37@gmail.com>
2024-04-20 11:08:06 +0700

    end of section 10

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { authGuard } from './_guards/auth.guard';
 import { TestsErrorComponent } from './errors/tests-error/tests-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   {
@@ -28,6 +30,11 @@ const routes: Routes = [
       {
         path: 'members/:username',
         component: MemberDetailComponent
+      },     
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        canDeactivate: [preventUnsavedChangesGuard]
       },     
       {
         path: 'lists',
